6e799a4d934bfa1a4162d126fbd69d6495b8f732
ICSC <ICSC@fundacion-jala.local>
2019-01-25 20:54:03 -0400

    adding browserstack support

--- e2e/environment/env/test.js
+++ e2e/environment/env/test.js
@@ -8,6 +8,14 @@ module.exports = {
     loginCredentials: {
         email: 'jafeth.garcia.j@gmail.com',
         password: 'Jys2laroca'
-    }
+    },
 
+    "browserstack": {
+        "user": "nairaromero2",
+        "key": "AuEpPDg9XzygDrQzrryK",
+        "os": "Windows",
+        "os_version": "10",
+        "browser": "Edge",
+        "browser_version": "18.0"
+      }
 };
