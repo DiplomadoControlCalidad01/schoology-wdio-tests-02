5bade913a288953e5c00caf63d4330783f70e393
Naira Romero Echeverria <Naira Romero@jalasoft.local>
2019-01-26 16:18:20 -0400

    adding course page info

--- e2e/environment/env/test.js
+++ e2e/environment/env/test.js
@@ -8,6 +8,12 @@ module.exports = {
     loginCredentials: {
         email: 'jafeth.garcia.j@gmail.com',
         password: 'Jys2laroca'
+    },
+    courseData: {
+        courseName: 'coursetest1',
+        nameSection: 'Sectiontest1',
+        tematicArea: 'physical education and health',
+        nivel: 'none'
     }
 
 };
